4181eb96a8bef8dfe6d0d941d078371d9e5b9171
ny64 <me@ny64.kr>
2025-02-06 09:22:50 +0900

    TouchableScale 컴포넌트 추가

---
+++ src/components/TouchableScale.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Animated, View} from 'react-native';
+
+interface Props {
+  pressInDuration?: number;
+  pressOutDuration?: number;
+  scaleTo?: number;
+  pressInEasing?: (t: number) => number;
+  pressOutEasing?: (t: number) => number;
+  onTouchEnd?: () => void;
+  style?: object;
+  children: React.ReactNode;
+}
+
+const TouchableScale = ({onTouchEnd, pressInDuration = 100, pressOutDuration = 100, scaleTo = 0.95, pressInEasing, pressOutEasing, style, children}: Props) => {
+  const scale = new Animated.Value(1);
+
+  const onPressIn = () => {
+    Animated.timing(scale, {
+      toValue: scaleTo,
+      duration: pressInDuration,
+      useNativeDriver: true,
+      easing: pressInEasing,
+    }).start();
+  };
+
+  const onPressOut = () => {
+    Animated.timing(scale, {
+      toValue: 1,
+      duration: pressOutDuration,
+      useNativeDriver: true,
+      easing: pressOutEasing,
+    }).start();
+  };
+
+  return (
+    <Animated.View style={{transform: [{scale}], ...style}} onTouchEnd={onTouchEnd}>
+      <View onTouchStart={onPressIn} onTouchEnd={onPressOut} onTouchCancel={onPressOut}>
+        {children}
+      </View>
+    </Animated.View>
+  );
+};
+
+export default TouchableScale;
