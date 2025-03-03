d442f22f6960cc1114c59fe9b30477bc403a04bd
ny64 <me@ny64.kr>
2025-03-04 01:04:04 +0900

    유저 정보 타입 추가

---
+++ src/types/user.ts
@@ -0,0 +1,13 @@
+export interface UserSchoolInfo {
+  schoolName: string;
+  comciganCode: number;
+  comciganRegion: string;
+  neisCode: string;
+  neisRegion: string;
+  neisRegionCode: string;
+}
+
+export interface UserClassInfo {
+  grade: string;
+  class: string;
+}
