29312a244a02c8b84e90bef82b8bb22e8d1ea9dc
ny64 <me@ny64.kr>
2025-03-05 14:35:34 +0900

    설정 페이지 상단 마진 추가

--- src/screens/Tab/Settings/index.tsx
+++ src/screens/Tab/Settings/index.tsx
@@ -14,7 +14,7 @@ const Settings = () => {
   return (
     <Container scrollView bounce>
       <TouchableWithoutFeedback onPress={() => setIsPressed(false)}>
-        <View style={{gap: 18, width: '100%'}}>
+        <View style={{gap: 18, width: '100%', marginTop: 16}}>
           <ProfileSection setIsPressed={setIsPressed} isPressed={isPressed} />
           <Card title="학교 정보 변경하기" arrow titleStyle={{fontSize: theme.typography.body.fontSize}} />
           <View style={{gap: 8}}>
