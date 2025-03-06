3c6a14b5a2dcc1c72f94e53e529c8031ea2dd6b5
ny64 <me@ny64.kr>
2025-03-06 18:52:38 +0900

    빌드 날짜 Date 객체로 변경

--- src/screens/Tab/Settings/components/AppInfoCard.tsx
+++ src/screens/Tab/Settings/components/AppInfoCard.tsx
@@ -8,7 +8,7 @@ import {theme} from '@/styles/theme';
 const AppInfoCard = () => {
   return (
     <Card title="앱 정보" titleStyle={{fontSize: theme.typography.body.fontSize}}>
-      <Content title="빌드 날짜" content={buildDate} />
+      <Content title="빌드 날짜" content={buildDate.format('YYYY년 M월 D일')} />
       <Content title="빌드 번호" content={appBuildNumber} />
       <Content title="버전" content={appVersion} />
     </Card>
