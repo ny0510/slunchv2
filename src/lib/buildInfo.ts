a69f72ecca61789f66bfa5ae6ffa83501a37422a
ny64 <me@ny64.kr>
2025-03-06 19:14:51 +0900

    v0.0.2 릴리즈

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -1,5 +1,6 @@
+import dayjs from 'dayjs';
 import DeviceInfo from 'react-native-device-info';
 
 export const appVersion = DeviceInfo.getVersion();
 export const appBuildNumber = DeviceInfo.getBuildNumber();
-export const buildDate = '2025년 3월 3일';
+export const buildDate = dayjs('2025-03-06');
