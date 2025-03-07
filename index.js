1b3afb38d80b10bf39c934b45b630bf222748bb6
ny64 <me@ny64.kr>
2025-03-07 10:01:43 +0900

    뒤로가기 두 번 눌러 종료

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
-import React from 'react';
-import {AppRegistry, Text, TextInput} from 'react-native';
+import React, {useEffect} from 'react';
+import {AppRegistry, BackHandler, Platform, Text, TextInput, ToastAndroid} from 'react-native';
 import {setCustomImage, setCustomText, setCustomTouchableOpacity} from 'react-native-global-props';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -18,11 +18,37 @@ TextInput.defaultProps = TextInput.defaultProps || {};
 TextInput.defaultProps.autoCorrect = false;
 TextInput.defaultProps.allowFontScaling = false;
 
+let backPressedOnce = false;
+
 const Root = () => {
   showSplash();
   enableScreens();
   changeNavigationBarColor('transparent', true);
   dayjs.locale('ko');
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      const backAction = () => {
+        if (backPressedOnce) {
+          BackHandler.exitApp();
+          return true;
+        }
+
+        backPressedOnce = true;
+        ToastAndroid.show('뒤로가기를 한 번 더 누르면 종료돼요.', ToastAndroid.SHORT);
+
+        setTimeout(() => {
+          backPressedOnce = false;
+        }, 2000);
+
+        return true;
+      };
+
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+      return () => backHandler.remove();
+    }
+  }, []);
 
   setCustomText({
     style: {
