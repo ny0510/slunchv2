36d0abf3268835a5ae8035fa7291fe79ecdf3900
ny64 <me@ny64.kr>
2025-03-12 09:59:50 +0900

    custom status validation 제거

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -6,9 +6,6 @@ const httpClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  validateStatus: status => {
-    return status < 500;
-  },
 });
 
 export default httpClient;
