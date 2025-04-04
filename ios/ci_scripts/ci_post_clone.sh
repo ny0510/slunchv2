09d7bb2c4e335b1c9857b04e66ea5bdb970cc8c9
ny64 <me@ny64.kr>
2025-04-04 19:40:06 +0900

    iOS ci post clone script 수정

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,19 @@
 #!/bin/zsh
 
+echo $SENTRY_PROPERTIES | base64 -di > ios/sentry.properties
+echo $GOOGLE_SERVICES_JSON | base64 -di > ios/GoogleService-Info.plist
+echo "API_BASE_URL='$API_BASE_URL'" > .env
+
+
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install node
 brew install cocoapods
 brew install yarn
 
+pwd
+cd ..
 yarn
+
+cd ios
 pod update hermes-engine --no-repo-update
 pod install
-
-echo $SENTRY_PROPERTIES | base64 -di > ios/sentry.properties
-echo $GOOGLE_SERVICES_JSON | base64 -di > ios/GoogleService-Info.plist
-echo "API_BASE_URL='$API_BASE_URL'" > .env
