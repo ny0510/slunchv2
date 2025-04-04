96efbb826cfbb6c5319fb735f788271152416c34
ny64 <me@ny64.kr>
2025-04-04 20:21:36 +0900

    iOS ci post clone script 수정 5차

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,12 +8,17 @@ echo $GOOGLE_SERVICES_JSON | base64 -di > ios/GoogleService-Info.plist
 echo "API_BASE_URL='$API_BASE_URL'" > .env
 
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install node
+brew install oven-sh/bun/bun
+brew install rbenv
+rbenv install 3.4.1
+rbenv global 3.4.1
+rbenv rehash
+rbenv exec gem install bundler -v 2.4.13
+rbenv exec gem install cocoapods -v 1.16.2
 #brew install cocoapods
-brew install yarn
-gem install cocoapods -v 1.16.2
+# gem install cocoapods -v 1.16.2
 
-yarn
+bun install
 
 cd ios
 pod update hermes-engine --no-repo-update
