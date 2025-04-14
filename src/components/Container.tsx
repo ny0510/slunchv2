3ef861b654963f6bb9827cce6b30bf8a670d0af0
ny64 <me@ny64.kr>
2025-04-15 08:33:29 +0900

    Container 컴포넌트 props rest로 최적화

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,32 +4,23 @@ import {ScrollView, StyleSheet, View} from 'react-native';
 interface Props {
   scrollView?: boolean;
   bounce?: boolean;
-  showsVerticalScrollIndicator?: boolean;
-  showsHorizontalScrollIndicator?: boolean;
   style?: {};
   children: React.ReactNode;
-  refreshControl?: React.ReactElement;
-  scrollViewRef?: React.RefObject<ScrollView>;
-  onScroll?: (event: any) => void;
 }
 
-const Container = ({scrollView = false, bounce = false, showsVerticalScrollIndicator = false, showsHorizontalScrollIndicator = false, style, children, refreshControl, scrollViewRef, onScroll}: Props) => {
+const Container = ({scrollView = false, bounce = false, style, children, ...rest}: Props & {[key: string]: any}) => {
   if (scrollView) {
     return (
-      <ScrollView
-        ref={scrollViewRef}
-        contentContainerStyle={[s.container, style]}
-        bounces={bounce}
-        showsVerticalScrollIndicator={showsVerticalScrollIndicator}
-        showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}
-        refreshControl={refreshControl}
-        onScroll={onScroll}
-        scrollEventThrottle={16}>
+      <ScrollView ref={rest.scrollViewRef} contentContainerStyle={[s.container, style]} bounces={bounce} {...rest}>
         {children}
       </ScrollView>
     );
   }
-  return <View style={[s.container, style]}>{children}</View>;
+  return (
+    <View style={[s.container, style]} {...rest}>
+      {children}
+    </View>
+  );
 };
 
 const s = StyleSheet.create({
