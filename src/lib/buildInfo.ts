b5196555170daa6402f70bfc83078f2b0438f107
ny64 <me@ny64.kr>
2025-04-23 20:05:10 +0900

    v0.5.0 릴리즈

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -3,4 +3,4 @@ import DeviceInfo from 'react-native-device-info';
 
 export const appVersion = DeviceInfo.getVersion();
 export const appBuildNumber = DeviceInfo.getBuildNumber();
-export const buildDate = dayjs('2025-04-12');
+export const buildDate = dayjs('2025-04-23');
