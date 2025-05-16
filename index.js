71d409e5d0647bcf22cda8d4adc6594ce36d38ec
ny64 <me@ny64.kr>
2025-05-16 14:01:04 +0900

    ATT 요청 추가

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs';
 import React, {useEffect} from 'react';
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry, AppState, Platform} from 'react-native';
 import mobileAds from 'react-native-google-mobile-ads';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
-import {PERMISSIONS, RESULTS, check, request} from 'react-native-permissions';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {enableScreens} from 'react-native-screens';
 import {showSplash} from 'react-native-splash-view';
+import {getTrackingStatus, requestTrackingPermission} from 'react-native-tracking-transparency';
 
 import {name as appName} from './app.json';
 import App from '@/App';
@@ -27,12 +27,32 @@ const Root = ({isHeadless}) => {
   }
 
   useEffect(() => {
-    async () => {
-      const result = await check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
-      if (result === RESULTS.DENIED) {
-        await request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
-      }
+    (async () => {
+      if (Platform.OS === 'ios') {
+        const state = await getTrackingStatus();
 
+        if (state === 'not-determined') {
+          const permission = await requestTrackingPermission();
+          if (permission === 'authorized') {
+            console.log('Tracking permission granted');
+          } else {
+            console.log('Tracking permission denied');
+          }
+        }
+        if (state === 'denied') {
+          console.log('Tracking permission denied');
+        } else if (state === 'authorized') {
+          console.log('Tracking permission granted');
+        }
+        if (state === 'restricted') {
+          console.log('Tracking permission restricted');
+        }
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    async () => {
       await mobileAds().setRequestConfiguration({testDeviceIdentifiers: ['EMULATOR']});
       const adapterStatuses = await mobileAds().initialize();
       console.log(`[AdMob] Adapter Statuses: ${JSON.stringify(adapterStatuses)}`);
