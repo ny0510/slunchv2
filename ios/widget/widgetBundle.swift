aa88090c3ea7bc74fee0be0860c96f2d8d30f0c7
ny64 <me@ny64.kr>
2025-12-05 16:59:32 +0900

    feat: iOS 위젯 초기화

---
+++ ios/widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by ny64 on 12/5/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
