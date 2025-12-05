b90971d0c0073ed9eda808954d01930951a1caea
imnyang <me@imnya.ng>
2025-12-05 17:51:24 +0900

    fix: update ci_post_clone.sh to install cocoapods and adjust environment variables

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -10,16 +10,18 @@ echo $GOOGLE_SERVICE_JSON | base64 -d > ios/GoogleService-Info.plist
 echo "API_BASE_URL='$API_BASE_URL'" > .env
 
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install oven-sh/bun/bun ruby node fastfetch
-echo 'export PATH="/opt/homebrew/opt/ruby/bin:$PATH"' >> /Users/neko/.zshrc
-export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
+brew install oven-sh/bun/bun ruby node fastfetch cocoapods
+
 export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
+export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/ruby/lib/pkgconfig"
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
 fastfetch
+
 gem install bundler -v 2.4.13
-gem install cocoapods -v 1.16.2
-#brew install cocoapods
+# gem install cocoapods -v 1.16.2
+# brew install cocoapods
 # gem install cocoapods -v 1.16.2
 
 bun install
